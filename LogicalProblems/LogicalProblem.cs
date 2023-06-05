86793f446d1206b4b39e52375eb57c7f70667e57
Akshay4955 <akshayshedge11@gmail.com>
2023-06-05 20:36:51 +0530

    Added: Code for Fibonacci series

---
+++ LogicalProblems/LogicalProblem.cs
@@ -0,0 +1,25 @@
+﻿namespace LogicalProblems;
+
+public class LogicalProblem
+{
+    public int FibonacciCalculator(int inputNumber)
+    {
+        Console.WriteLine("Fibonacci series upto " + inputNumber + " numbers");
+        int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+        for (int i = 1; i <= inputNumber; i++)
+        {
+            if (i == 1 || i == 2)
+            {
+                Console.Write(fibo1 + " ");
+            }
+            else
+            {
+                fibonacci = fibo1 + fibo2;
+                fibo1 = fibo2;
+                fibo2 = fibonacci;
+                Console.Write(fibonacci + " ");
+            }
+        }
+        return fibonacci;
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -5,5 +5,10 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to logical problems...!!");
+        LogicalProblem logicalProblem = new LogicalProblem();
+        Console.WriteLine("Please enter number of fibonacci required");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+        logicalProblem.FibonacciCalculator(inputNumber);
+
     }
 }
